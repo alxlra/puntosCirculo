3d6ac0b9f2aeec5018dd9c53e715899a4a421281
GitHub Action deploy <action@github.com>
2024-10-05 00:49:49 +0000

    Publicación de rama dev

--- src/app.py
+++ src/app.py
@@ -5,28 +5,28 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 # Función para generar puntos en el perímetro del círculo
-def calcular_puntos(center_x, center_y, radio, puntos):
+def calcular_puntos(center_x, center_y, center_z, radio, puntos):
     """Calcula los puntos en un círculo basado en las coordenadas del centro, el radio y la cantidad de puntos."""
     angulo_lados = (2 * math.pi) / puntos
-    df = pd.DataFrame(columns=["X", "Y"])
+    df = pd.DataFrame(columns=["X", "Y", "Z"])
 
     for i in range(puntos):
         x = center_x + radio * math.cos(angulo_lados * i)
         y = center_y + radio * math.sin(angulo_lados * i)
-        df.loc[len(df)] = [x, y]
+        df.loc[len(df)] = [x, y, center_z]
 
     return df
 
 # Función para calcular las distancias de movimiento entre los puntos
-def calcular_distancias(df, puntos):
+def calcular_distancias(df, z, puntos):
     """Calcula las distancias de movimiento entre cada punto."""
-    df_dist = pd.DataFrame(columns=["Movimiento X", "Movimiento Y"])
-    df_dist.loc[len(df_dist)] = [df.loc[0]["X"], df.loc[0]["Y"]]  # Movimiento inicial
+    df_dist = pd.DataFrame(columns=["Movimiento X", "Movimiento Y", "Movimiento Z"])
+    df_dist.loc[len(df_dist)] = [df.loc[0]["X"], df.loc[0]["Y"], z]  # Movimiento inicial
 
     for i in range(len(df)):
         dist_x = df.loc[(i + 1) % puntos]["X"] - df.loc[i]["X"]
         dist_y = df.loc[(i + 1) % puntos]["Y"] - df.loc[i]["Y"]
-        df_dist.loc[len(df_dist)] = [dist_x, dist_y]
+        df_dist.loc[len(df_dist)] = [dist_x, dist_y, z]
 
     return df_dist
 
@@ -41,16 +41,14 @@ def calcular_distancias_motor(df, z, ini_a, ini_b, ini_c):
     for i in range(len(df)):
         dist_x = df.loc[(i + 1) % puntos]["X"] - df.loc[i]["X"]
         dist_y = df.loc[(i + 1) % puntos]["Y"] - df.loc[i]["Y"]
-        #dist_z = df.loc[(i + 1) % puntos]["Z"] - df.loc[i]["Z"]
-        df_dist.loc[len(df_dist)] = [dist_x+dist_y, dist_x-dist_y, dist_x-z]
+        dist_z = df.loc[(i + 1) % puntos]["Z"] - df.loc[i]["Z"]
+        df_dist.loc[len(df_dist)] = [dist_x+dist_y, dist_x-dist_y, dist_x- dist_z]
 
     # Regreso al punto inicial
-    last = len(df_dist)-1
-    #df_dist.loc[len(df_dist)] = [dist_x - ini_a, dist_y- ini_b, 0- ini_c] 
     i=0
     df_dist.loc[len(df_dist)] = [-(df.loc[i]["X"] + df.loc[i]["Y"] - ini_a), -(df.loc[i]["X"] - df.loc[i]["Y"] - ini_b), -(df.loc[i]["X"]-z - ini_c)] 
 
-
+    #código G
     df_dist["Movimiento X"] = df_dist["Movimiento X"]*0.2
     df_dist["Movimiento Y"] = df_dist["Movimiento Y"]*(-0.2)
     df_dist["Movimiento Z"] = df_dist["Movimiento Z"]*(-0.2)
@@ -77,33 +75,33 @@ st.title("📐 Generador de puntos de un polígono")
 
 col1,col2 = st.columns(2)
 with col1:
-    center_x = st.number_input("Coordenada X del centro:", min_value=2.0, max_value=30.0, value=21.83, format="%.2f")
-    center_y = st.number_input("Coordenada Y del centro:", min_value=2.0, max_value=30.0, value=12.66, format="%.2f")
-    center_z = st.number_input("Altura:", min_value=2.0, max_value=30.0, value=5.625, format="%.2f")
+    center_x = st.number_input("Coordenada X del centro:", min_value=2.0, max_value=30.0, value=21.83, format="%.3f", step=0.5)
+    center_y = st.number_input("Coordenada Y del centro:", min_value=2.0, max_value=30.0, value=12.66, format="%.3f", step=0.5)
+    center_z = st.number_input("Altura:", min_value=2.0, max_value=30.0, value=5.625, format="%.3f", step=0.5)
 with col2:
-    ini_a = st.number_input("Offset carro A:", min_value=0.0, max_value=30.0, value=0.0, format="%.2f")
-    ini_b = st.number_input("Offset carro B:", min_value=0.0, max_value=30.0, value=16.25, format="%.2f")
-    ini_c = st.number_input("Offset carro C:", min_value=0.0, max_value=30.0, value=2.5, format="%.2f")
+    ini_a = st.number_input("Offset carro A:", min_value=0.0, max_value=30.0, value=0.0, format="%.3f")
+    ini_b = st.number_input("Offset carro B:", min_value=0.0, max_value=30.0, value=16.25, format="%.3f")
+    ini_c = st.number_input("Offset carro C:", min_value=0.0, max_value=30.0, value=2.5, format="%.3f")
 
-radio = st.number_input("Radio:", min_value=1.0, max_value=10.0, value=4.0, format="%.2f")
+radio = st.number_input("Radio:", min_value=1.0, max_value=10.0, value=4.0, format="%.2f", step=0.5)
 puntos = st.slider("Número de puntos:", min_value=3, max_value=30, value=15)
 
 
 #puntos = st.slider("Número de puntos:", min_value=3, max_value=30, value=15)
 
 if "df" not in st.session_state:
-    st.session_state.df = pd.DataFrame(columns=["X", "Y"])
+    st.session_state.df = pd.DataFrame(columns=["X", "Y", "Z"])
 
 if "df_dist" not in st.session_state:
-    st.session_state.df_dist = pd.DataFrame(columns=["Movimiento X", "Movimiento Y"])
+    st.session_state.df_dist = pd.DataFrame(columns=["Movimiento X", "Movimiento Y", "Movimiento Z"])
 
 if "df_dist_motor" not in st.session_state:
     st.session_state.df_dist_motor = pd.DataFrame(columns=["Movimiento X", "Movimiento Y", "Movimiento Z"])
 
 if st.button("✔ Generar puntos"):
     #cálculos
-    st.session_state.df = calcular_puntos(center_x, center_y, radio, puntos)    
-    st.session_state.df_dist = calcular_distancias(st.session_state.df, puntos)
+    st.session_state.df = calcular_puntos(center_x, center_y, center_z, radio, puntos)    
+    st.session_state.df_dist = calcular_distancias(st.session_state.df,center_z, puntos)
     st.session_state.df_dist_motor = calcular_distancias_motor(st.session_state.df, center_z, ini_b, ini_a, ini_c)
 
 # ------------
@@ -142,7 +140,7 @@ if not st.session_state.df.empty and not st.session_state.df_dist.empty:
                 mime="text/csv"
             )
         with col3:
-            st.subheader("Carros")
+            st.subheader("Carros código G")
             st.dataframe(df_dist_motor)
             #st.button("Descargar movimientos", df.to_csv("movimientos.csv", index=False))
             csv3 = df_dist_motor.to_csv(index=False)  # Convertir el dataframe a CSV
